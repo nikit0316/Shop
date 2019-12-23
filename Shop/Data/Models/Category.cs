9115e50a781543a7968e9973c87d971a89ba16da
Treskov Nikita <treskov-1@mail.ru>
2019-12-23 18:40:27 +0300

    First one

---
+++ Shop/Data/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class Category
+    {
+        public int id { set; get; }
+        public string categoryname { set; get; }
+        public string desc { set; get; }
+        public List<Car> cars { set; get; }
+    }
+}
---
+++ Shop/Data/mocks/MockCars.cs
@@ -0,0 +1,49 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.mocks
+{
+    public class MockCars : IAllCars
+    {
+        private readonly ICarsCategory _categoryCars = new MockCategory();
+        public IEnumerable<Car> Cars {
+            get
+            {
+                return new List<Car>
+                {
+                    new Car {
+                        name ="Tesla Model S",
+                        shortDesc="Быстрый автомобиль",
+                        longDesc="Матушка природа будет тобой довольна",
+                        img="/img/model s.jpg",
+                        price=45000,
+                        isFavourite=true,
+                        available=true,
+                        Category= _categoryCars.AllCategories.First()
+                    },
+                    new Car
+                    {
+                        name="BMW M3",
+                        shortDesc="Дерзкий и стильный",
+                        longDesc="Удобный автомобиль для городской жизни",
+                        img="/img/m3.jpg",
+                        price=65000,
+                        isFavourite=true,
+                        available=true,
+                        Category=_categoryCars.AllCategories.Last()
+                    }
+                };
+            }
+        }
+        public IEnumerable<Car> getFavCars { get; set; }
+
+        public Car getObjectCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
