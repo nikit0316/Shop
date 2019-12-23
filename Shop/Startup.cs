920489cb5eed3fc221279c97567bc3b50a2081b5
Treskov Nikita <treskov-1@mail.ru>
2019-12-24 02:24:46 +0300

    Half of the entity

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Shop.Data;
 using Shop.Data.interfaces;
 using Shop.Data.mocks;
 
@@ -14,10 +16,18 @@ namespace Shop
 {
     public class Startup
     {
+
+        private IConfigurationRoot _confString;
+        public Startup(IHostingEnvironment hostEnv)
+        {
+            _confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("DbSettings.json").Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confString.GetConnectionString("DefaultCOnnection")));
             services.AddTransient<IAllCars, MockCars>();
             services.AddTransient<ICarsCategory, MockCategory>();
             services.AddMvc(option => option.EnableEndpointRouting = false);
