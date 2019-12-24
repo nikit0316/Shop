077528e437e8c42ef9bff51f7e5c92e9e7bfc001
Bogdan Goncharenko <beg.beg42@gmail.com>
2019-12-24 04:43:20 +0300

    Added ShopCartItem

---
+++ Shop/Data/Models/ShopCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Data.Models
+{
+    public class ShopCartItem
+    {
+        public int itemId { get; set; }
+        public Car car { get; set; }
+        private int price { get; set; }
+        public string ShopCartId { get; set; }
+    }
+}
