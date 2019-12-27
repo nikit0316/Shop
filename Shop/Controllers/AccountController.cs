d7de221f5a46a5c150c0c16a4a0a18e456720a3b
Treskov Nikita <treskov-1@mail.ru>
2019-12-27 07:44:01 +0300

    Registration

---
+++ Shop/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Shop.Data.Models;
+
+namespace CustomIdentityApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<Users> _userManager;
+        private readonly SignInManager<Users> _signInManager;
+
+        public AccountController(UserManager<Users> userManager, SignInManager<Users> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users user = new Users { Email = model.Email, UserName = model.Email, Year=model.Year};
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
