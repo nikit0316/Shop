ec528f78086a5d831a5bc7b9609fe610c9169cb6
Nikita Treskov <treskov-1@mail.ru>
2019-12-30 01:02:37 +0300

    Roles

---
+++ Shop/Controllers/UsersController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Models;
+using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserManager<Users> _userManager;
+
+        public UsersController(UserManager<Users> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index() => View(_userManager.Users.ToList());
+        public IActionResult Create() => View();
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserVIewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users user = new Users { Email = model.Email, UserName = model.Email, Year = model.Year };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Edit(string id)
+        {
+            Users user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, Year = user.Year };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.Year = model.Year;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            Users user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Shop/RoleInitializer.cs
@@ -0,0 +1,32 @@
+﻿using Shop.Data.Models; 
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace CustomIdentityApp
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<Users> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "_Aa123456";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("employee") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("employee"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                Users admin = new Users { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
